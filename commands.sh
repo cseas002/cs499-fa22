a4da3b97c3b12ca56a7369e21ce83abc27b1e63b
cseas002 <cseas002@node0.lab-04-websearch.ucy-cs499-dc-pg0.clemson.cloudlab.us>
2022-09-27 09:17:39 -0400

    added ansible to commands.sh

--- commands.sh
+++ commands.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd 
-git clone git@github.com:ucy-coast/cs499-fa22.git
 sudo apt-get -y update 
 ssh node1 sudo apt-get -y update
 sudo cp ./assignments/hw1/metrics/DRAM/Linux/mlc /bin/
+sudo apt-get -y install ansible
